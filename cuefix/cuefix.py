3359af82bcff76429c1d793586b0c51d0ad4f743
Jian Li <lijianxp2005@gmail.com>
2020-08-21 18:02:41 -0400

    add some utils for file codec detection

--- cuefix/cuefix.py
+++ cuefix/cuefix.py
@@ -1,2 +1,21 @@
 import chardet
 
+
+def scan_folder():
+    pass
+
+
+def detect_file_encoding(byte_str, filename):
+    encoding = chardet.detect(byte_str)
+    if encoding['confidence'] < 0.75:
+        raise ValueError('Cannot detect encoding of file {filename}: {encoding}'.format(
+            filename=filename, encoding=encoding))
+    return encoding['encoding']
+
+
+def encoding_to_utf8(byte_str, filename):
+    return byte_str.
+
+
+def newline_to_lf(filename):
+    pass
