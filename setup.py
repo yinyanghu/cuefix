4e86f568bdbf888012c59e3761203110d6e49603
Jian Li <lijianxp2005@gmail.com>
2020-08-27 03:02:18 -0400

    update classifiers

--- setup.py
+++ setup.py
@@ -22,6 +22,10 @@ setup(
     ],
     classifiers=[
         'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
     ],
