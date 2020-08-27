736a4512b56c0715a0ed619e4b8227dd93453b8a
Jian Li <lijianxp2005@gmail.com>
2020-08-27 02:50:51 -0400

    group options

--- cuefix/cli.py
+++ cuefix/cli.py
@@ -3,41 +3,47 @@ import cuefix
 import sys
 
 
+def validate_args(args):
+    pass
+
+
 def main():
     parser = argparse.ArgumentParser(
         prog='cuefix',
         description='Clean and fix a CUE file in a directory',
     )
     parser.add_argument('filepath', help='file path to the input cue file')
-
     parser.add_argument('--version', action='version',
                         version='%(prog)s {0}'.format(cuefix.__version__))
-
-    parser.add_argument('-i', '--info', action='store_true', default=False,
-                        help='display metainfo of the input cue file only')
-
-    parser.add_argument('-e', '--encoding', default='utf-8-sig',
-                        help='encoding which cue file will be converted to, default is UTF-8 BOM')
-
-    parser.add_argument('-n', '--newline', default='unix',
-                        help='newline format which cue file will be converted to, default is Unix')
-
-    parser.add_argument('--no-encoding', action='store_true', default=False,
-                        help='converting encoding will be skipped')
-
-    parser.add_argument('--no-newline', action='store_true', default=False,
-                        help='converting newline will be skipped')
-
-    parser.add_argument('--no-backup', action='store_true', default=False,
-                        help='no backup for the input cue file, USE WITH CAUTION!')
-
-    parser.add_argument('--dryrun', action='store_true', default=False,
-                        help='dry-run and print out fixed cue file')
-
     parser.add_argument('-v', '--verbose', action='store_true', default=False,
                         help='print version and exit')
 
+    dryrun_group = parser.add_argument_group('dry-run options')
+    dryrun_group.add_argument('-i', '--info', action='store_true', default=False,
+                              help='display metainfo of the input cue file only')
+    dryrun_group.add_argument('--dryrun', action='store_true', default=False,
+                              help='dry-run and print out fixed cue file')
+
+    fix_group = parser.add_argument_group('fix options')
+    fix_group.add_argument('-e', '--encoding', default='utf-8-sig',
+                           choices=['utf-8-sig', 'utf-8', 'gb2312'],
+                           help='encoding which cue file will be converted to, default is UTF-8 BOM')
+    fix_group.add_argument('-n', '--newline', default='unix',
+                           choices=['unix', 'windows'],
+                           help='newline format which cue file will be converted to, default is Unix')
+    fix_group.add_argument('--no-encoding', action='store_true', default=False,
+                           help='converting encoding will be skipped')
+    fix_group.add_argument('--no-newline', action='store_true', default=False,
+                           help='converting newline will be skipped')
+
+    backup_group = parser.add_argument_group('backup options')
+    backup_group.add_argument('--no-backup', action='store_true', default=False,
+                              help='no backup for the input cue file, USE WITH CAUTION!')
+
     args = parser.parse_args()
+
+    validate_args(args)
+
     if args.info:
         print(cuefix.info(args.filepath))
         return
